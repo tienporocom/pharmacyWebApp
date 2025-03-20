d8d17c035aa3383dfb6fee1cf92c694c7c624780
Nguyen Van Tien <tienporocom@gmail.com>
2025-03-20 22:40:36 +0700

    Add reset CSS, faviconIcon, header-footer example, DB2Import

---
+++ frontend/src/assets/js/main.js
@@ -0,0 +1,16 @@
+// Hàm để import nội dung của file HTML vào phần tử có id tương ứng
+function includeHTML() {
+    document.querySelectorAll("[data-include]").forEach(element => {
+        let file = element.getAttribute("data-include");
+
+        fetch(file)
+            .then(response => response.text())
+            .then(data => {
+                element.innerHTML = data;
+            })
+            .catch(error => console.error("Lỗi khi tải file: " + file, error));
+    });
+}
+
+// Gọi hàm sau khi trang đã load
+document.addEventListener("DOMContentLoaded", includeHTML);
