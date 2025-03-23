cfc8859873ad2a409f6622f4505ab6cf4a33f089
Nguyen Van Tien <tienporocom@gmail.com>
2025-03-23 09:26:25 +0700

    Add link to register

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,36 +1,14 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI);
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
-const mongoose = require("mongoose");
-require("dotenv").config(); // Đảm bảo dotenv được gọi
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    console.log("MONGO_URI:", process.env.MONGO_URI); // Debug
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined");
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
     }
-
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
 };
 
 module.exports = connectDB;
+
